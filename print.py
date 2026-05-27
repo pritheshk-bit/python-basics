057b5107ee4314bd76b4f224be4a90e5bbf69eda
pritheshk-bit <pritheshk@ekyaschools.com>
2026-05-27 16:47:25 +0530

    geting started

--- print.py
+++ print.py
@@ -1,4 +1,14 @@
-print("hallo wrold")
-print("my name is prithesh")
-print("the graed i am in is 7 graed")
-print(5-7,"is a negative number")
+# print a sentence
+print("welcome to the world of programing")
+
+#print a number
+print(6)
+
+#next line after print
+print("hello python \n")
+
+#pass multiple argument for printing
+print("hello",5)
+
+#end argument of print statement
+print("welcome to ",end= "*")
---
+++ variables.py
@@ -0,0 +1,13 @@
+#creting different variables
+x = 5
+y = "john"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#user input
+name = input("enter your name: ")
+
+print("\n Hello" , name, "\nwelcome to codingal")
