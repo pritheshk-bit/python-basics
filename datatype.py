fb40d64109d792a3ed267a24a74d665f6927033b
pritheshk-bit <pritheshk@ekyaschools.com>
2026-05-31 12:50:11 +0530

    data types

---
+++ datatype.py
@@ -0,0 +1,11 @@
+a =5 
+print("types of a : " , type(a))
+
+b=2.5
+print("types of b : " , type(b))
+
+c = "coding"
+print("types of c : " , type(c))
+
+d= True
+print("types of d : " , type(d))
---
+++ typecasting.py
@@ -0,0 +1,18 @@
+a =5 
+print("types of a : " , type(a))
+
+b=2.5
+print("types of b : " , type(b))
+
+c = "coding"
+print("types of c : " , type(c))
+
+d= True
+print("types of d : " , type(d))
+print("\n after type casting.... ")
+age = str(b)
+print(age)
+print("data type of age is" ,type(age))
+weight =int(b)
+print(weight)
+print("data type of weight is" , type(weight)) 
