35e43ca68479302d0911792fa4a3b29aa20fe328
pritheshk-bit <pritheshk@ekyaschools.com>
2026-05-31 12:53:50 +0530

    Printing the title.py

---
+++ o.py
@@ -0,0 +1,17 @@
+# Printing the title
+print("My Daily Routine")
+
+# Step 1: Waking up
+print("I wake up at 6:30 am.")
+
+# Step 2: Going to school
+print("I go to school at 7:15 am")
+
+# Step 3: Playing after school
+print("i go to basketball at 5:00 evening and come 6:30 .")
+
+# Step 4: Studying
+print("I study for 1 hours and 30min  at night.")
+
+# Calculating total study time in a week
+print("Total study hours in a week" , 6 )
