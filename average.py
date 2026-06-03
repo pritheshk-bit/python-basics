24a2bc41a26a14f7bd75e124db17be69108399be
pritheshk-bit <pritheshk@ekyaschools.com>
2026-06-03 16:49:51 +0530

    py,operators

---
+++ average.py
@@ -0,0 +1,14 @@
+#storing values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+#finding the total of trees 
+sum = tree1+tree2+tree3+tree4+tree5
+print("the average of all the tree is : " ,sum) 
+
+#finding the average of tree
+average = sum/5
+print("the average of all the tree is :" , average)
---
+++ count.py
@@ -0,0 +1,10 @@
+Amount =int(input("please enter amount for withdraw :"))
+
+note_1 = Amount//100
+note_2 = (Amount%100)//50
+note_3 = ((Amount%100)%50)//10
+
+
+print("note of 100 rupee"  , note_1)
+print("note of 50 rupee"  , note_2)
+print("note of 10 rupee"  , note_3)
