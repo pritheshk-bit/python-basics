b54b228dd88f5dd7351ae48b05f2cdd72b89f157
pritheshk-bit <pritheshk@ekyaschools.com>
2026-06-04 16:43:29 +0530

    conditional statements

---
+++ greter.py
@@ -0,0 +1,8 @@
+i = int(input("enter a number : "))
+if(i <= 15):
+    print("i is samller than 15")
+    print("i'm in if Block")
+else:
+    print("i is greter that 15")
+    print("i'm in eles Block")
+print("i'm not  in if  and not in else block")
