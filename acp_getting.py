5eb52debc0ca2eab1004dd2b3697c530c669fb8b
pritheshk-bit <pritheshk@ekyaschools.com>
2026-06-09 15:50:27 +0530

    Add files via upload

---
+++ acp_getting.py
@@ -0,0 +1,11 @@
+raj = "12 January"
+amit = "5 March"
+priya = "20 June"
+sneha = "14 September"
+arun = "30 November"
+
+print("Raj's birthday is", raj)
+print("Amit's birthday is", amit)
+print("Priya's birthday is", priya)
+print("Sneha's birthday is", sneha)
+print("Arun's birthday is", arun)
