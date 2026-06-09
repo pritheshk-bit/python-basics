fcb52d8309a41516a0f1ccd409ca9f8b40b3b2d2
pritheshk-bit <pritheshk@ekyaschools.com>
2026-06-09 15:59:24 +0530

    Add files via upload

---
+++ Summer times.py
@@ -0,0 +1,6 @@
+temp = int(input("Enter today's temperature: "))
+
+if temp > 24:
+    print("It is hot today. Wear light and soft clothes.")
+else:
+    print("It is cold today. Wear a jacket or pullover.")
