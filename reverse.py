37b97067be13f3bc657a3f02365735994333f869
pritheshk-bit <pritheshk@ekyaschools.com>
2026-06-25 16:27:08 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,10 @@
+
+string = input("please enter your own name: ")
+
+string2 = ''
+
+for i in string:
+    string2 = i + string2
+
+print("\nthe Orginal name", string)
+print ("the Reversed name", string2)
---
+++ sum.py
@@ -0,0 +1,6 @@
+n = int(input("enetr the number whose sum you wnat to find: "))
+sum = 0
+
+for i in range(1, n+1):
+    sum = sum+i
+    print("\nsum =",sum)
